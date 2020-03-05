98fae12bb19ffd5276b768f10585f9f4037f1fd2
Tony Nguyen <42789742+tonyponyexpress@users.noreply.github.com>
2020-03-04 20:56:06 -0600

    added reverseGraph

--- scc_t828n219.py
+++ scc_t828n219.py
@@ -2,30 +2,32 @@
 # KUID: 2878004
 # Class: EECS 660 Algorithms
 # Brief: SCC Algorithm with BFS.
+
 import sys
 from collections import defaultdict
 
 class Graph:
     def __init__( self, dict, t_nodes ):
-        self.Graph = dict
+        self.graph = dict
         self.totalNodes = t_nodes
         self.BFSResults = list()
 
-    def BFS( self, source ):
+    # Using BFS to implement SCC
+    def BFS( self, start ):
         # Initializations
         discovered = [ False for i in range( self.totalNodes ) ]
         discovered[ 0 ] = True # As soon as search first sees v
-        for item in self.Graph[ source ]:
+        for item in self.graph[ start ]:
             discovered.append( False )
         L = list()
         BFS = list()
-        L.append( [ source ] )
+        L.append( [ start ] )
         i = 0
 
         # Traversals
         for index in range( len( L[ i ] ) ):
             for item in L[ i ]:
-                edge = self.Graph[ item ]
+                edge = self.graph[ item ]
                 u = int( item )
                 v = int( edge[ 0 ] )
                 if False == discovered[ v ]:
@@ -34,6 +36,14 @@ class Graph:
                     L.append( [ v ] ) # Add v to the list
             i+=1
         self.BFSResults = BFS
+
+    def reverseGraph( self ):
+        g = Graph( self.graph, self.totalNodes )
+        for index in list( self.graph ):
+            for subindex in self.graph[ index ]:
+                self.graph[ subindex ].append( index )
+        return g
+
 
 def Main():
     File = open( sys.argv[ 1 ], 'r' )
@@ -45,8 +55,10 @@ def Main():
         for item in temp:
             Edges[ index + 1 ].append( item )
     G = Graph( Edges, TotalNodes )
-    G.BFS( 1 )
-    print( G.BFSResults )
+    
 
+    # G.BFS( 1 )
+    # print( G.BFSResults )
+    # print( G.reverseGraph() )
 # Call to main
 Main()
